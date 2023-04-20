fa5826433ed0c9c8c1bdab83e9dfe5c19f0cba4f
rhit-halseysh <halseysh@rose-hulman.edu>
2023-04-20 13:59:34 -0400

    Adding starting skeleton

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,32 +3,78 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Spencer Halsey, Khai Phung
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
+rhit.FB_KEY_USERID = "id";
+rhit.FB_KEY_USERNAME = "userName";
+rhit.FB_KEY_PASSWORD = "password";
+rhit.FB_KEY_SIGN = "sign";
+rhit.FB_KEY_LAST_LOGIN = "lastLogIn";
+rhit.fbImageManager = null;
+rhit.fbImageDetailManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
-rhit.ClassName = class {
+rhit.PageController = class {
+
 	constructor() {
+		if (this.constructor == Employee) {
+			throw new Error(" Object of Abstract Class cannot be created");
+		}
+	}
+	updateView() {
+		throw new Error("Abstract Method has no implementation");
+	}
+
+}
+
+rhit.LandingPageController = class extends PageController  {
+
+	constructor() {
+		pageSwitchButtons();
+	}
+
+	pageSwitchButtons(){
 
 	}
 
-	methodName() {
+}
 
-	}
+rhit.LoginPageController = class extends PageController  {
+
+}
+
+rhit.MainPageController = class extends PageController  {
+
+}
+
+rhit.HoroscopePageController = class extends PageController  {
+
+}
+
+rhit.HoroscopeManager = class {
+
+}
+
+rhit.DetailPageController = class extends PageController  {
+
+}
+
+rhit.ProfilePageController = class extends PageController  {
+
 }
 
 /* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
 };
