0de71a830c6c26678b22134eac6a630443f676bb
Khai Phung <phunga@rose-hulman.edu>
2023-05-18 01:59:47 -0400

    Some refactoring

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -48,6 +48,8 @@ class LandingPageController {
 			console.log("Moving")
 			window.location.href = "/login.html";
 		};
+
+		new SplitPageController();
 	}
 
 }
@@ -599,10 +601,10 @@ class EightballPageController {
 					let responseNumber = Math.floor(Math.random() * 20) + 1;
 					let responseText = text[responseNumber].answer;
 
-					if(document.querySelector("#reponseText").textContent == responseText){
-						 responseNumber = Math.floor(Math.random() * 20) + 1;
-						 responseText = text[responseNumber].answer;
-					}	
+					if (document.querySelector("#reponseText").textContent == responseText) {
+						responseNumber = Math.floor(Math.random() * 20) + 1;
+						responseText = text[responseNumber].answer;
+					}
 
 					document.querySelector("#reponseText").textContent = responseText;
 
@@ -615,49 +617,53 @@ class EightballPageController {
 
 }
 //SPLIT Page
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const landingPage = document.querySelector('#landingPage');
+class SplitPageController {
+	constructor() {
+		const left = document.querySelector('.left');
+		const right = document.querySelector('.right');
+		const landingPage = document.querySelector('#landingPage');
 
-left.addEventListener('mouseenter', ()=>{
-	landingPage.classList.add('hover-left')
-});
+		left.addEventListener('mouseenter', () => {
+			landingPage.classList.add('hover-left')
+		});
 
-left.addEventListener('mouseleave', ()=>{
-	landingPage.classList.remove('hover-left')
-});
+		left.addEventListener('mouseleave', () => {
+			landingPage.classList.remove('hover-left')
+		});
 
-right.addEventListener('mouseenter', ()=>{
-	landingPage.classList.add('hover-right')
-});
+		right.addEventListener('mouseenter', () => {
+			landingPage.classList.add('hover-right')
+		});
 
-right.addEventListener('mouseleave', ()=>{
-	landingPage.classList.remove('hover-right')
-});
+		right.addEventListener('mouseleave', () => {
+			landingPage.classList.remove('hover-right')
+		});
 
-const head = document.querySelector('#header');
-const content = document.querySelector('#content');
-const b1 =  document.querySelector('#about');
-const b2 =  document.querySelector('#about1');
-const b3 =  document.querySelector('#about2');
+		const head = document.querySelector('#header');
+		const content = document.querySelector('#content');
+		const b1 = document.querySelector('#about');
+		const b2 = document.querySelector('#about1');
+		const b3 = document.querySelector('#about2');
 
-b1.addEventListener('click', ()=>{
-	head.innerHTML = "About Us";
-	content.innerHTML = "This Application is developed by\nKhai Phung and Spencer Halsey, \nfounders of Fortune280 LLC.";
-});
+		b1.addEventListener('click', () => {
+			head.innerHTML = "About Us";
+			content.innerHTML = "This Application is developed by\nKhai Phung and Spencer Halsey, \nfounders of Fortune280 LLC.";
+		});
 
-b2.addEventListener('click', ()=>{
-	head.innerHTML = "Our Mission";
-	content.innerHTML = `With our app Rflekt™ (patent pending) we hope to create a safe space for self discovery and growth for everyone.  Our app aims to be the preeminent self discovery help app on the web, helping everyone to “Reflect, Transform, Thrive”.`;
-});
+		b2.addEventListener('click', () => {
+			head.innerHTML = "Our Mission";
+			content.innerHTML = `With our app Rflekt™ (patent pending) we hope to create a safe space for self discovery and growth for everyone.  Our app aims to be the preeminent self discovery help app on the web, helping everyone to “Reflect, Transform, Thrive”.`;
+		});
 
 
-b3.addEventListener('click', ()=>{
-	head.innerHTML = "Our App";
-	content.innerHTML = `Rflekt is a self discovery website. It will provide the user a variety of information to help them understand themselves and plan their day`;
-});
+		b3.addEventListener('click', () => {
+			head.innerHTML = "Our App";
+			content.innerHTML = `Rflekt is a self discovery website. It will provide the user a variety of information to help them understand themselves and plan their day`;
+		});
 
-//END SPLIT PAGE
+		//END SPLIT PAGE
+	}
+}
 
 /* Main */
 function main() {
@@ -669,6 +675,7 @@ function main() {
 		if (document.querySelector("#landingPage")) {
 			console.log("On the landing page");
 			new LandingPageController();
+			new SplitPageController();
 		} else if (document.querySelector("#loginPage")) {
 			console.log("On the login page");
 			new LoginPageController();
